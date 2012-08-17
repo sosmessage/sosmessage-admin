31172bc26a3104d887142dca724d1d2284d6ad29
Thomas Roger <throger@gmail.com>
2012-08-18 00:02:51 +0200

    Fix compilation

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object ApplicationBuild extends Build {
       "com.mongodb.casbah" %% "casbah" % "3.0.0-SNAPSHOT",
       "net.liftweb" %% "lift-json" % "2.4-M4",
       "commons-lang" % "commons-lang" % "2.6"
-
+    )
 
     val buildSettings =
       Seq(
