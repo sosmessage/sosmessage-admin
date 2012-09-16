dd60b495f4d30e777b661fa08f126e7b9ffeef8d
Thomas Roger <throger@gmail.com>
2012-09-17 01:27:04 +0200

    AJAX refresh of existing stats

--- public/javascripts/statistics.js
+++ public/javascripts/statistics.js
@@ -1,17 +1,38 @@
 $(document).ready(function() {
-  jsRoutes.controllers.Statistics.requestsStats().ajax({
-      success : function(data) {
-        $("#stats-requests").html(data)
-      }
-  });
-  jsRoutes.controllers.Statistics.usersStats().ajax({
-      success : function(data) {
-        $("#stats-users").html(data)
-      }
-  });
-  jsRoutes.controllers.Statistics.appsStats().ajax({
-      success : function(data) {
-        $("#stats-apps").html(data)
-      }
-  });
+  setInterval(function() {
+    var ele = $("#stats-requests")
+    var loading = ele.find(".js-loading")
+    loading.show()
+    jsRoutes.controllers.Statistics.requestsStats().ajax({
+        success : function(data) {
+          ele.find(".js-stats-content").html(data)
+          loading.hide()
+        }
+    })
+  }, 5000)
+
+  setInterval(function() {
+    var ele = $("#stats-users")
+    var loading = ele.find(".js-loading")
+    loading.show()
+    jsRoutes.controllers.Statistics.usersStats().ajax({
+        success : function(data) {
+          ele.find(".js-stats-content").html(data)
+          loading.hide()
+        }
+    })
+  }, 5000)
+
+  setInterval(function() {
+    var ele = $("#stats-apps")
+    var loading = ele.find(".js-loading")
+    loading.show()
+    jsRoutes.controllers.Statistics.appsStats().ajax({
+        success : function(data) {
+          ele.find(".js-stats-content").html(data)
+          loading.hide()
+        }
+    })
+  }, 5000)
+
 });
