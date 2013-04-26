c4ab8ee8fbfd04215f063c0277f879fa2136d8d1
Thomas Roger <throger@gmail.com>
2013-04-26 19:54:07 +0200

    Release 2.2

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
     val appName         = "sosmessage-admin"
-    val appVersion      = "2.2-SNAPSHOT"
+    val appVersion      = "2.2"
 
     val appDependencies = Seq(
       "org.mongodb" %% "casbah" % "2.5.0",
